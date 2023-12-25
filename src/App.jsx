5892b6ba8c35363faabc5a67f1112881eead3d9c
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2023-12-25 23:39:57 +0300

    Show view pictures and css changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,31 @@ function getRandomView() {
 
 function App() {
   const [view, setView] = useState([]);
+
   const showView = () => {
     setView([...view, getRandomView()]);
   };
+
   const viewList = view.map((view, index) => {
     return <View key={index} viewName={view} />;
   });
 
   return (
     <>
-      <button onClick={showView}>Show The View</button>
-      <div>{viewList}</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 my-5 d-flex justify-content-center">
+            <button className="btn btn-primary" onClick={showView}>
+              Show The View
+            </button>
+          </div>
+          <div className="col-12">
+            <div className="d-flex flex-wrap justify-content-center">
+              {viewList}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/View.jsx
+++ src/View.jsx
@@ -4,10 +4,17 @@ import mountainView from "./images/mountain-view.png";
 import seaView from "./images/sea-view.png";
 import summerView from "./images/summer-view.webp";
 
+const viewMap = {
+  City: cityView,
+  Mountain: mountainView,
+  Sea: seaView,
+  Summer: summerView,
+};
+
 function View({ viewName }) {
   return (
     <div>
-      <img src={cityView} alt="City View" />
+      <img src={viewMap[viewName]} alt="City View" className="mb-2 me-3" />
     </div>
   );
 }
