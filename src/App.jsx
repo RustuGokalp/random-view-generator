5cdbb29e3ea89d3fe2dee7393f6785d62775919e
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2023-12-25 23:59:25 +0300

    Add delete button and it's condition

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,10 @@ function App() {
     setView([...view, getRandomView()]);
   };
 
+  const deleteView = () => {
+    setView([]);
+  };
+
   const viewList = view.map((view, index) => {
     return <View key={index} viewName={view} />;
   });
@@ -26,6 +30,11 @@ function App() {
             <button className="btn btn-primary" onClick={showView}>
               Show The View
             </button>
+            {view.length > 0 && (
+              <button className="btn btn-danger ms-5" onClick={deleteView}>
+                Delete View(s)
+              </button>
+            )}
           </div>
           <div className="col-12">
             <div className="d-flex flex-wrap justify-content-center">
